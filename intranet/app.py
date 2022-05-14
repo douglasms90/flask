19ce5f8ab8b0cf4a36db9d3cc6f80b1a5bcfff6a
douglasms90 <douglasms90@gmail.com>
2022-05-13 22:34:17 -0300

    templates

--- intranet/app.py
+++ intranet/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import flask_excel as excel
 
 from intranet.ext import config
 from intranet.ext import appearance
@@ -10,5 +9,5 @@ app = Flask(__name__)
 
 config.init_app(app)
 appearance.init_app(app)
-database.init_app(app)
-views.init_app(app)
+db = database.init_app(app)
+views.init_app(app, db)
--- intranet/ext/database.py
+++ intranet/ext/database.py
@@ -5,19 +5,19 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 
 
 def init_app(app):
-  db = None    
-  def __init__(self):
-    self.db = psycopg2.connect("dbname='' user='' host='' password=''")
+  db = psycopg2.connect("dbname='mkData3.0' user='' host='' password=''")
 
-  def consult(self, select):
-    db = None 
-    try:
-      cur = self.db.cursor()
-      cur.execute(select)
-      db = cur.fetchall()
-      return db
-    except:
-      return "Impossible to connect to the database, check your code."
-  
-  def close(self):
-    self.db.close()
+  try:
+    cur = db.cursor()
+    cur.execute("""SELECT os.codos, df.descricao_defeito, tp.descricao, os.data_abertura, cl.nome_razaosocial, cd.cidade, ba.bairro
+      FROM mk_os os
+      FULL OUTER JOIN mk_os_tipo tp ON os.tipo_os = tp.codostipo
+      JOIN mk_pessoas cl ON os.cliente = cl.codpessoa
+      JOIN mk_os_defeitos df ON os.defeito_associado = df.coddefeito
+      JOIN mk_cidades cd ON os.cd_cidade = cd.codcidade
+      JOIN mk_bairros ba ON os.cd_bairro = ba.codbairro
+      WHERE status='1' AND tipo_os in ('4','15','18') AND fechamento_tecnico='N' ORDER BY cd.cidade asc""")
+    db = cur.fetchall()
+    return db
+  except:
+    return "Impossible to connect to the database, check your code."
