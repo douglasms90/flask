7c861be3f0fa57431bb3813ab834933414e40fab
douglas <douglasms90@gmail.com>
2022-10-16 17:37:31 -0300

    for title index

--- intranet/blueprints/webui/views.py
+++ intranet/blueprints/webui/views.py
@@ -18,8 +18,8 @@ def index():
       JOIN mk_cidades cd ON os.cd_cidade = cd.codcidade
       JOIN mk_bairros ba ON os.cd_bairro = ba.codbairro
       WHERE status='1' AND tipo_os in ('4','15','18') AND fechamento_tecnico='N' ORDER BY cd.cidade asc""")
-    print(rows)
-    return render_template("index.html", form=form, rows = rows)
+    title = ('ID', 'DEFEITO', 'TIPO', 'GERADA', 'CLIENTE', 'CIDADE', 'BAIRRO', 'DESCRIÇÃO') 
+    return render_template("index.html", form=form, rows = rows, ts = title)
   if form.comm.data == 're':
     rows = conn.consult("""SELECT os.codos, df.descricao_defeito, tp.descricao, os.data_fechamento, cl.nome_razaosocial, os.operador_fech_tecnico, os.servico_prestado
       FROM mk_os os
@@ -27,7 +27,8 @@ def index():
       JOIN mk_pessoas cl ON os.cliente = cl.codpessoa
       JOIN mk_os_defeitos df ON os.defeito_associado = df.coddefeito
       WHERE  tipo_os in ('4','15','18') AND data_fechamento = CURRENT_DATE AND servico_prestado LIKE '%ugl%' ORDER BY os.data_fechamento desc""")
-    return render_template("index.html", form=form, rows = rows)
+    title = ('ID', 'DEFEITO', 'DESCRIÇÃO', 'FECHAMENTO', 'CLIENTE', 'OPERADOR', 'DESCRIÇÃO')
+    return render_template("index.html", form=form, rows = rows, ts = title)
   if request.method == "POST":
     tomorrow = date.today() + timedelta(days=1)
     return excel.make_response_from_array(rows, "xlsx", file_name = tomorrow.strftime('%d.%m.%Y'))
