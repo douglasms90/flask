2d81385f45eb64bf417106a9d00518b947c58b0f
douglasms <douglasms90@gmail.com>
2022-10-22 10:38:04 -0300

    date format and -excel

--- intranet/blueprints/webui/views.py
+++ intranet/blueprints/webui/views.py
@@ -1,6 +1,4 @@
-from flask import abort, render_template, request
-from datetime import date, timedelta
-import flask_excel as excel
+from flask import abort, render_template
 
 from intranet.ext.database import dbConn
 from intranet.ext.forms import commForms
@@ -10,7 +8,7 @@ def index():
   form = commForms()
   conn = dbConn()
   if form.comm.data == 'os':
-    rows = conn.consult("""SELECT os.codos, df.descricao_defeito, tp.descricao, os.data_abertura, cl.nome_razaosocial, cd.cidade, ba.bairro,  os.defeito_reclamado
+    rows = conn.consult("""SELECT os.codos, df.descricao_defeito, tp.descricao, to_char(os.data_abertura, 'dd/mm/yyyy'), cl.nome_razaosocial, cd.cidade, ba.bairro
       FROM mk_os os
       FULL OUTER JOIN mk_os_tipo tp ON os.tipo_os = tp.codostipo
       JOIN mk_pessoas cl ON os.cliente = cl.codpessoa
@@ -18,10 +16,10 @@ def index():
       JOIN mk_cidades cd ON os.cd_cidade = cd.codcidade
       JOIN mk_bairros ba ON os.cd_bairro = ba.codbairro
       WHERE status='1' AND tipo_os in ('4','15','18') AND fechamento_tecnico='N' ORDER BY cd.cidade asc""")
-    title = ('ID', 'DEFEITO', 'TIPO', 'GERADA', 'CLIENTE', 'CIDADE', 'BAIRRO', 'DESCRIÇÃO') 
+    title = ('ID', 'DEFEITO', 'TIPO', 'GERADA', 'CLIENTE', 'CIDADE', 'BAIRRO') 
     return render_template("index.html", form=form, rows = rows, ts = title)
   if form.comm.data == 're':
-    rows = conn.consult("""SELECT os.codos, df.descricao_defeito, tp.descricao, os.data_fechamento, cl.nome_razaosocial, os.operador_fech_tecnico, os.servico_prestado
+    rows = conn.consult("""SELECT os.codos, df.descricao_defeito, tp.descricao, to_char(os.data_fechamento, 'dd/mm/yyyy'), cl.nome_razaosocial, os.operador_fech_tecnico, os.servico_prestado
       FROM mk_os os
       FULL OUTER JOIN mk_os_tipo tp ON os.tipo_os = tp.codostipo
       JOIN mk_pessoas cl ON os.cliente = cl.codpessoa
@@ -29,7 +27,4 @@ def index():
       WHERE  tipo_os in ('4','15','18') AND data_fechamento = CURRENT_DATE AND servico_prestado LIKE '%ugl%' ORDER BY os.data_fechamento desc""")
     title = ('ID', 'DEFEITO', 'DESCRIÇÃO', 'FECHAMENTO', 'CLIENTE', 'OPERADOR', 'DESCRIÇÃO')
     return render_template("index.html", form=form, rows = rows, ts = title)
-  if request.method == "POST":
-    tomorrow = date.today() + timedelta(days=1)
-    return excel.make_response_from_array(rows, "xlsx", file_name = tomorrow.strftime('%d.%m.%Y'))
   return render_template("index.html", form=form)
