9e94d32366a998807a8589da4d5f58adc4d6f119
douglas <douglasms90@gmail.com>
2023-01-02 16:38:23 -0300

    -infoDB

--- intranet/blueprints/webui/views.py
+++ intranet/blueprints/webui/views.py
@@ -8,23 +8,11 @@ def index():
   form = commForms()
   conn = dbConn()
   if form.comm.data == 'os':
-    rows = conn.consult("""SELECT os.codos, df.descricao_defeito, tp.descricao, to_char(os.data_abertura, 'dd/mm/yyyy'), cl.nome_razaosocial, cd.cidade, ba.bairro
-      FROM mk_os os
-      FULL OUTER JOIN mk_os_tipo tp ON os.tipo_os = tp.codostipo
-      JOIN mk_pessoas cl ON os.cliente = cl.codpessoa
-      JOIN mk_os_defeitos df ON os.defeito_associado = df.coddefeito
-      JOIN mk_cidades cd ON os.cd_cidade = cd.codcidade
-      JOIN mk_bairros ba ON os.cd_bairro = ba.codbairro
-      WHERE status='1' AND tipo_os in ('4','15','18') AND fechamento_tecnico='N' ORDER BY cd.cidade asc""")
+    rows = conn.consult("""""")
     title = ('ID', 'DEFEITO', 'TIPO', 'GERADA', 'CLIENTE', 'CIDADE', 'BAIRRO') 
     return render_template("index.html", form=form, rows = rows, ts = title)
   if form.comm.data == 're':
-    rows = conn.consult("""SELECT os.codos, df.descricao_defeito, tp.descricao, to_char(os.data_fechamento, 'dd/mm/yyyy'), cl.nome_razaosocial, os.operador_fech_tecnico, os.servico_prestado
-      FROM mk_os os
-      FULL OUTER JOIN mk_os_tipo tp ON os.tipo_os = tp.codostipo
-      JOIN mk_pessoas cl ON os.cliente = cl.codpessoa
-      JOIN mk_os_defeitos df ON os.defeito_associado = df.coddefeito
-      WHERE  tipo_os in ('4','15','18') AND data_fechamento = CURRENT_DATE AND servico_prestado LIKE '%ugl%' ORDER BY os.data_fechamento desc""")
+    rows = conn.consult("""""")
     title = ('ID', 'DEFEITO', 'DESCRIÇÃO', 'FECHAMENTO', 'CLIENTE', 'OPERADOR', 'DESCRIÇÃO')
     return render_template("index.html", form=form, rows = rows, ts = title)
   return render_template("index.html", form=form)
--- intranet/ext/database.py
+++ intranet/ext/database.py
@@ -7,7 +7,7 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 class dbConn():
   conn = None
   def __init__(self):
-    self.conn = psycopg2.connect("dbname='mkData3.0' user='cliente_r' host='177.184.72.6' password='Cl13nt_R'")
+    self.conn = psycopg2.connect("")
 
   def consult(self, query):
     try:
