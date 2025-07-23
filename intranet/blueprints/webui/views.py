3045285a040007665d473d7c93e0b4b1ee8531b9
douglasms90 <douglasms90@gmail.com>
2025-07-23 09:16:10 -0300

    nails

--- intranet/blueprints/webui/views.py
+++ intranet/blueprints/webui/views.py
@@ -3,6 +3,7 @@ from intranet.models import MessageForm
 
 def index():
     form = MessageForm()
+    booked_hours = ["12:00", "13:00"]  # exemplo vindo do servidor
     if form.validate_on_submit():
-        return render_template("table.html", form=form)
-    return render_template("index.html", form=form)
+        return render_template("index.html", form=form)
+    return render_template('index.html', booked_hours=booked_hours)
