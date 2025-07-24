3a341b321fee56673af90707e8c7dc346869158a
douglasms90 <douglasms90@gmail.com>
2025-07-23 23:23:32 -0300

    nails

--- intranet/blueprints/webui/__init__.py
+++ intranet/blueprints/webui/__init__.py
@@ -4,8 +4,8 @@ from .views import index, hour
 
 bp = Blueprint("webui", __name__, template_folder="templates")
 
-bp.add_url_rule("/", methods = ["get", "post"], view_func=index)
-bp.add_url_rule("/hour/<hour_id>", view_func=hour, endpoint="hourview")
+bp.add_url_rule("/", view_func=index)
+bp.add_url_rule("/hour/<hour>", view_func=hour, endpoint="hourview")
 
 def init_app(app):
     app.register_blueprint(bp)
--- intranet/blueprints/webui/views.py
+++ intranet/blueprints/webui/views.py
@@ -2,11 +2,8 @@ from flask import render_template, redirect, url_for
 from intranet.models import MessageForm
 
 def index():
-    booked_hours = ["11:00", "12:00"]  # exemplo vindo do servidor
-    return render_template('index.html', booked_hours=booked_hours, date=[8, 9, 10, 11, 12])
+    booked_hours = ["11:00", "12:00", "13:00"]  # exemplo vindo do servidor
+    return render_template('index.html', booked_hours=booked_hours)
 
-def hour():
-    form = MessageForm()
-    if form.validate_on_submit():
-        return render_template("hour.html")
-    return render_template('hour.html')
+def hour(hour):
+    return render_template('hour.html', formatted_hour=hour)
